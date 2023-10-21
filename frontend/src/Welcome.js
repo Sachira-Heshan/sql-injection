0cbdbe9ba6bf2f4f250b01ad755710c1691873cd
Sachira Heshan <sachiraheshan99@gmail.com>
2023-10-22 00:29:24 +0530

    added welcome page

---
+++ frontend/src/Welcome.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Welcome = () => {
+    return (
+        <div style={{
+            width: "100%",
+            height: "100vh",
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <h3 style={{
+                textAlign: 'center'
+            }}>Welcome</h3>
+        </div>
+    )
+}
+
+export default Welcome
